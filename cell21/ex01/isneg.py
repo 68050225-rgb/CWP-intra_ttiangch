226bea54e6eeb769896bbcaced4b0b891e9fa2fe
68050225-rgb <68050225@kmitl.ac.th>
2026-09-24 10:28:43 +0700

    Delete cell21/ex01 directory

--- cell21/ex01/isneg.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-if n<0: 
-    print("This number is negative.")
-elif n>0: 
-    print("This number is positive.")
-else: 
-    print("This number is both positive and negative.")
- 
