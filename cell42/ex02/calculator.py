3824ca37ad9d458dd0f6538400571c9d84af4810
68050225-rgb <68050225@kmitl.ac.th>
2026-09-24 10:39:38 +0700

    Delete cell42/ex02 directory

--- cell42/ex02/calculator.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input("Give me the first number: "))
-n2 = int(input("Give me the second number: "))
-print("Thank you!")
-print(f"{n1} + {n2} = {n1 + n2}")
-print(f"{n1} - {n2} = {n1 - n2}")
-print(f"{n1} / {n2} = {n1 // n2}")
-print(f"{n1} * {n2} = {n1 * n2}")
