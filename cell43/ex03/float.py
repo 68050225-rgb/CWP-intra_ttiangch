60b309dd0a56e2bd8536fb5ec2dca4dfe63442a5
68050225-rgb <68050225@kmitl.ac.th>
2026-09-24 10:40:52 +0700

    Delete cell43/ex03 directory

--- cell43/ex03/float.py
+++
@@ -1,9 +0,0 @@
-u = input("Give me a number: ")
-try:
-    n = float(u)
-    if n.is_integer():
-        print("This number is an integer.")
-    else:
-        print("This number is a decimal.")
-except ValueError:
-    pass
